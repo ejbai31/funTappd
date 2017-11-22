ccd037f557fae5e31d92e6adbbb79020c8bd07c8
Edward Bai <edwardbai31@gmail.com>
2017-11-22 11:17:36 -0800

    assign store config to entry and add session/errors reducers

--- frontend/funTappd.jsx
+++ frontend/funTappd.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/store';
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore(store);
     ReactDOM.render(<h1>BORKBORKBORK</h1>, root);
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
-
-window.login = login;
-window.signup = signup;
-window.logout = logout;
