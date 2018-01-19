fcf45601af84c009ce84d5f0015cce5e6c732983
Edward Bai <edwardbai31@gmail.com>
2018-01-18 16:48:00 -0800

    Test react-rating npm package

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,7 +6,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Dashboard from './dashboard/dashboard_container';
 import BeerContainer from './beers/beer_index_container';
 import BeerFormContainer from './beers/beer_form_container';
-import BeerReviewsContainer from './reviews/beer_reviews';
+import BeerReviewsContainer from './reviews/beer_review_container';
 
 const App = () => (
   <div>
@@ -18,7 +18,6 @@ const App = () => (
       <ProtectedRoute path="/create_beer" component={BeerFormContainer} />
       <ProtectedRoute path= "/beers/:id/edit" component={BeerFormContainer} />
       <ProtectedRoute path= "/beers/:id/reviews" component={BeerReviewsContainer} />
-    </Switch>
     </Switch>
     <footer>
       {/* TODO
