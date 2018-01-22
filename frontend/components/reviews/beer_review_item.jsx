c36457e6b59f4f1789615decabc54054bf0aeadc
Edward Bai <edwardbai31@gmail.com>
2018-01-21 16:11:26 -0800

    Add beer review skeleton

---
+++ frontend/components/reviews/beer_review_item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class BeerReviewItem extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>Beer Reviews</h3>
+      </div>
+    );
+  }
+}
+
+export default BeerReviewItem;
