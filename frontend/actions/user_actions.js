38ee04c83636313b36795b5ca266e9a4edaa0a46
Edward Bai <edwardbai31@gmail.com>
2018-01-24 21:31:25 -0800

    Add user actions for updated stats

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,9 +1,15 @@
 import * as UserUtil from '../util/user_api_util';
 
 export const RECEIVE_USER = "RECEIVE_USER";
+export const UPDATE_USER = "UPDATE_USER";
 
 export const receive_user = user => ({
   type: RECEIVE_USER, 
+  user
+});
+
+export const update_user = user => ({
+  type: UPDATE_USER, 
   user
 });
 
