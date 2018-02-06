606c55e16a8cda5e84ebc0a4826f3e43a944c9a0
Edward Bai <edwardbai31@gmail.com>
2018-02-05 23:44:11 -0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do 
-    resources :users, only: [:show, :create, :update, :edit] do
+    resources :users, only: [:show, :create, :update, :edit, :index] do
       resources :beer_reviews, only: [:index]
     end
     resources :beers, only: [:show, :create, :destroy, :index, :update, :edit] do
       resources :beer_reviews, only: [:index]
     end
-    resources :breweries, only: [:create]
+    resources :breweries, only: [:create, :index]
     resources :beer_reviews, only: [:create, :show, :update, :destroy, :index]
     # get 'api/reviews' => 'beer_reviews#show'
     resource :session, only: [ :show, :create, :destroy]
