fcc5a3f49085a6bf9fbda91d2940c0cf4c767633
Edward Bai <edwardbai31@gmail.com>
2018-02-14 19:02:24 -0800

    Add user index to reducer

--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -1,10 +1,12 @@
 import merge from 'lodash/merge';
-import { RECEIVE_USER } from '../actions/user_actions';
+import { RECEIVE_USER, RECEIVE_USERS } from '../actions/user_actions';
 
 const UserReducer = (state=[], action) => {
   Object.freeze(state);
   let newState = merge({}, state);
   switch(action.type){
+    case RECEIVE_USERS:
+      return merge({}, action.users);
     case RECEIVE_USER:
       return merge({}, state, {[action.user.id]: action.user});
     default:
