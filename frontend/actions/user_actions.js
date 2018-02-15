7abbb8280581192ce2692051bab690fadeec0d43
Edward Bai <edwardbai31@gmail.com>
2018-02-14 19:00:24 -0800

    Add user index api util and actions

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -2,18 +2,28 @@ import * as UserUtil from '../util/user_api_util';
 
 export const RECEIVE_USER = "RECEIVE_USER";
 export const UPDATE_USER = "UPDATE_USER";
+export const RECEIVE_USERS = "RECEIVE_USER";
 
-export const receive_user = user => ({
+export const receiveUser = user => ({
   type: RECEIVE_USER, 
   user
 });
 
-export const update_user = user => ({
+export const updateUser = user => ({
   type: UPDATE_USER, 
   user
+});
+
+export const receiveUsers = () => ({
+  type: RECEIVE_USERS
 });
 
 export const getUser = id => dispatch => (
   UserUtil.fetchUser(id)
     .then(user => dispatch(getUser(user)))
+);
+
+export const getUsers = () => dispatch => (
+  UserUtil.fetchUsers()
+    .then(beers => dispatch(receiveUsers(beers)))
 );
